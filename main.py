dea0bc8022c656044d6c3f7536d6320f42895163
Lakshmeesh-H <lakshmeesh.h@gmail.com>
2020-06-06 11:05:36 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import re
 
-string = 'Search this inside of this text please!'
+# string = 'Search this inside of this text please!'
 
-pattern = re.compile('this')
+# pattern = re.compile('this')
 
 # result = re.search('this', string)
 # result = pattern.search(string)
 # result = pattern.findall(string)
 # result = pattern.fullmatch(string)
-result = pattern.match(string)
+# result = pattern.match(string)
 
 # print(result.span())
 # print(result.start())
-print(result)
+# print(result)
+
+password = 'hoadahqehqh8%'
+pattern = re.compile(r"[A-Za-z0-9$%#@]{8,}\d")
+check = pattern.fullmatch(password)
+print(check)
+
+# At least 8 characters long 
+# contains any letter, digits and $%#@
+# has to end with a number 
